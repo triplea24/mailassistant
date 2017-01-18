ebe209f6a8fad85aa187158abce432349c8b7a8a
Amir Ahangari <triplea1373@gmail.com>
2017-01-18 14:58:47 +0330

    Try to add views

--- panel/views.py
+++ panel/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+	# t = loader.get_template('index.html')
+    # c = {'foo': 'bar'}
+    # return HttpResponse(t.render(c, request), content_type='application/xhtml+xml')
+    return HttpResponse("This is the first page")
+@login_required
 def panel(request):
 	return HttpResponse("Panel")
