1550a02e5a72f2c8b50bf6b8f527797277dcbd2f
Amir Ahangari <triplea1373@gmail.com>
2017-01-18 17:10:38 +0330

    Farzam

--- mailasist/urls.py
+++ mailasist/urls.py
@@ -37,11 +37,7 @@ urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^track/', include('tracker.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    # url(r'^dashboard',include('panel.urls')),
     url(r'^',include('panel.urls')),
-    # url(r'^dashboard',include('panel.urls')),
-    # url(r'^dashboard/',include('panel.urls')),
-    # url(r'^dashboard',include('panel.urls')),
     url(r'^login/$', auth_views_html.login, name='login'),
     url(r'^logout/$', auth_views_html.logout,{'next_page': '/'}, name='logout'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
