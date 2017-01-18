8a3cd9d8e1773320dc6e2b88fa1581b6def8683a
Amir Ahangari <triplea1373@gmail.com>
2017-01-18 17:17:08 +0330

    Majid

--- panel/views.py
+++ panel/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .models import Mail,Log,Receiver
+from tracker.models import Mail,Log,Receiver
 # Create your views here.
 
 @login_required(login_url = '/login/')
