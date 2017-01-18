f2e9b052698b45d8639952bd63f97531916b621c
Amir Ahangari <triplea1373@gmail.com>
2017-01-18 16:17:35 +0330

    Gand

--- panel/views.py
+++ panel/views.py
@@ -11,22 +11,22 @@ def index(request):
     # return HttpResponse(t.render(c, request), content_type='application/xhtml+xml')
     return HttpResponse("This is the first page")
 
-def register(request):
-	if request.method == 'POST':
-        form = RegistrationForm(request.POST)
-        if form.is_valid():
-            user = User.objects.create_user(
-            username=form.cleaned_data['username'],
-            password=form.cleaned_data['password1'],
-            email=form.cleaned_data['email']
-            )
-            return HttpResponseRedirect('/register/success/')
-    else:
-        form = RegistrationForm()
-	    variables = RequestContext(request, {
-	    'form': form
-	    })
-    return render_to_response('registration/register.html',variables,)
+# def register(request):
+# 	if request.method == 'POST':
+#         form = RegistrationForm(request.POST)
+#         if form.is_valid():
+#             user = User.objects.create_user(
+#             username=form.cleaned_data['username'],
+#             password=form.cleaned_data['password1'],
+#             email=form.cleaned_data['email']
+#             )
+#             return HttpResponseRedirect('/register/success/')
+#     else:
+#         form = RegistrationForm()
+# 	    variables = RequestContext(request, {
+# 	    'form': form
+# 	    })
+#     return render_to_response('registration/register.html',variables,)
 
 @login_required(login_url = '/login/')
 def panel(request):
