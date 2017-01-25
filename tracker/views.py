25695584892eec12f2a28a8dfdb360527917d5a0
Hossein Rahmati <hossein.rahmati@engineer.com>
2017-01-25 15:11:29 +0000

    A

--- tracker/views.py
+++ tracker/views.py
@@ -21,24 +21,24 @@ def new_mail(request):
         subject = request.GET['subject']
         count = 0
         track_key = request.GET['track_key']
-            
+
         with transaction.atomic():
-            mail = Mail(sender = user , timestamp = timestamp ,count = count , track_key = track_key)
+            mail = Mail(sender = user , subject = subject , timestamp = timestamp ,count = count , track_key = track_key)
             mail.save()
             tos = request.GET['tos']
             for to in tos:
-                email = to['email']
-                receiver = Receiver(mail = mail, email = email, type_of_receiption = 'T')
+                # email = to['email']
+                receiver = Receiver(mail = mail, email = to, type_of_receiption = 'T')
                 receiver.save()
             ccs = request.GET['ccs']
             for cc in ccs:
-                email = cc['email']
-                receiver = Receiver(mail = mail, email = email, type_of_receiption = 'C')
+                # email = cc['email']
+                receiver = Receiver(mail = mail, email = cc, type_of_receiption = 'C')
                 receiver.save()
             bccs = request.GET['ccs']
             for bcc in bccs:
-                email = bcc['email']
-                receiver = Receiver(mail = mail, email = email, type_of_receiption = 'B')
+                # email = bcc['email']
+                receiver = Receiver(mail = mail, email = bcc, type_of_receiption = 'B')
                 receiver.save()
 
         return HttpResponse('')
